3358719d3bcc6b52845d1730f69ba3fc3b34e288
Tyler Draughon <tdraughon@gmail.com>
2015-11-18 10:42:12 -0500

    home page without navbar, selected page changes in navbar

--- app/js/app.js
+++ app/js/app.js
@@ -6,8 +6,7 @@ var App = React.createClass({
   render: function() {
     return (
       <div>
-        <NavBar />
-        {this.props.children || <Home />}
+        { this.props.children || <Home />}
       </div>
     );
   }
@@ -16,27 +15,25 @@ var App = React.createClass({
 var NavBar = React.createClass({displayName: 'NavBar',
   render: function() {
     return (
-      React.createElement('div', {className: "navBar"},
-        <nav className="navbar navbar-default">
-		  <div className="container-fluid">
-		    <div className="navbar-header">
-		      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-		        <span className="sr-only">Toggle navigation</span>
-		        <span className="icon-bar"></span>
-		        <span className="icon-bar"></span>
-		        <span className="icon-bar"></span>
-		      </button>
-		      <a className="navbar-brand" href="#">Brand</a>
-		    </div>
-		    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		      <ul className="nav navbar-nav">
-		        <li className="active"><Link to="#">Home <span className="sr-only">(current)</span></Link></li>
-		        <li><Link to="p2">Page Two</Link></li>
-		      </ul>
-		    </div>
-		  </div>
-		</nav>
-      )
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">Brand</a>
+        </div>
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav">
+            <li className="active" id="home"><Link to="#">Home <span className="sr-only">(current)</span></Link></li>
+            <li id="p2link"><Link to="p2">Page Two</Link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     );
   }
 });
@@ -45,15 +42,27 @@ var NavBar = React.createClass({displayName: 'NavBar',
 var Home = React.createClass({
     render: function() {
       return (
-        <h1>Home</h1>
+        <div>
+          <h1>Home</h1>
+          <li><Link to="p2">Page Dos</Link></li>
+        </div>
       );
     }
 });
 
 var PageTwo = React.createClass({
+
+  componentDidMount: function() {
+    $(".nav").find(".active").removeClass("active");
+    $("#p2link").addClass("active");
+    
+  },
   render: function() {
     return (
-      <h1>Page Two</h1>
+      <div>
+        <NavBar />
+        <h1>Page Two</h1>
+      </div>
     );
   }
 });
