e6b7caaf2009f95043791521e6e4c0fd8bb22445
Tyler Draughon <tdraughon@gmail.com>
2015-11-19 11:16:02 -0500

    All works except proptypes

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,37 @@ var Router = ReactRouter.Router;
 var Link = ReactRouter.Link;
 var Route = ReactRouter.Route;
 
+
+
 var App = React.createClass({
+  // context so the componevnt can access the router
+  contextTypes: {
+      router: React.PropTypes.func.isRequired
+  },
+
+  // initial state
+  getInitialState: function() {
+      return {
+        // the user is logged in
+          loggedIn: auth.loggedIn()
+      };
+  },
+
+  // callback when user is logged in
+  setStateOnAuth: function(loggedIn) {
+      this.state.loggedIn = loggedIn;
+  },
+
+  // when the component loads, setup the callback
+  componentWillMount: function() {
+      auth.onChange = this.setStateOnAuth;
+  },
+
+  // logout the user and redirect to home page
+  logout: function(event) {
+      auth.logout();
+      this.context.router.replaceWith('/');
+  },
   render: function() {
     return (
       <div>
@@ -40,8 +70,63 @@ var NavBar = React.createClass({displayName: 'NavBar',
 
 
 var Login = React.createClass({
+  // context so the component can access the router
+  contextTypes: {
+      router: React.PropTypes.func.isRequired
+  },
 
+  // initial state
+  getInitialState: function() {
+      return {
+          // there was an error on logging in
+          error: false
+      };
 
+  },
+
+  register: function(event) {
+    // prevent default browser submit
+        event.preventDefault();
+        // get data from form
+        var name = this.refs.name.value;
+        var username = this.refs.username.value;
+        var password = this.refs.password.value;
+        var confpass = this.refs.confirmpassword.value;
+        var classkey = this.refs.classkey.value;
+        if (!name || !username || !password || !confpass || !classkey) {
+            return;
+        }
+        if(password !== confpass)
+          return;
+        // register via the API
+        auth.register(name, username, password, classkey, function(loggedIn) {
+            // register callback
+            if (!loggedIn)
+                return this.setState({
+                    error: true
+                });
+            Route.transitionTo('/student');
+        }.bind(this));
+  },
+  login: function(event) {
+        // prevent default browser submit
+        event.preventDefault();
+        // get data from form
+        var username = this.refs.loginusername.value;
+        var password = this.refs.loginpassword.value;
+        if (!username || !password) {
+            return;
+        }
+        // login via API
+        auth.login(username, password, function(loggedIn) {
+            // login callback
+            if (!loggedIn)
+                return this.setState({
+                    error: true
+                });
+            Route.transitionTo('/student');
+        }.bind(this));
+  },
   componentDidMount: function() {
     $("#login-form").fadeIn(1);
     $("#register-form").fadeOut(1);
@@ -84,21 +169,21 @@ var Login = React.createClass({
                   <div className="panel-body">
                     <div className="row">
                       <div className="col-lg-12">
-                        <form id="login-form" action="" method="post" role="form" >
+                        <form id="login-form" onSubmit={this.login}>
                           <div className="form-group">
-                            <input type="text" name="username" id="username" tabIndex="1" className="form-control" placeholder="Username" />
+                            <input type="text" ref="loginusername" id="loginusername" tabIndex="1" className="form-control" placeholder="Username" />
                           </div>
                           <div className="form-group">
-                            <input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" />
+                            <input type="password" ref="loginpassword" id="loginpassword" tabIndex="2" className="form-control" placeholder="Password" />
                           </div>
                           <div className="form-group text-center">
-                            <input type="checkbox" tabIndex="3" className="" name="remember" id="remember" />
+                            <input type="checkbox" tabIndex="3" className="" ref="remember" id="remember" />
                             <label htmlFor="remember"> Remember Me</label>
                           </div>
                           <div className="form-group">
                             <div className="row">
                               <div className="col-sm-6 col-sm-offset-3">
-                                <input type="submit" name="login-submit" id="login-submit" tabIndex="4" className="form-control btn btn-login" value="Log In" />
+                                <input type="submit" ref="login-submit" id="login-submit" tabIndex="4" className="form-control btn btn-login" value="Log In" />
                               </div>
                             </div>
                           </div>
@@ -111,27 +196,36 @@ var Login = React.createClass({
                               </div>
                             </div>
                           </div>
+                          {this.state.error ? (
+                          <div className="alert">Invalid username or password.</div>
+                          ) : null }
                         </form>
-                        <form id="register-form" action="" method="post" role="form" >
+                        <form id="register-form" onSubmit={this.register}>
                           <div className="form-group">
-                            <input type="text" name="username" id="username" tabIndex="1" className="form-control" placeholder="Username" />
+                            <input type="text" ref="name" id="name" tabIndex="1" className="form-control" placeholder="Name" />
                           </div>
                           <div className="form-group">
-                            <input type="email" name="email" id="email" tabIndex="1" className="form-control" placeholder="Email Address" />
+                            <input type="text" ref="username" id="username" tabIndex="2" className="form-control" placeholder="Username (BYU NetID)" />
                           </div>
                           <div className="form-group">
-                            <input type="password" name="password" id="password" tabIndex="2" className="form-control" placeholder="Password" />
+                            <input type="password" ref="password" id="password" tabIndex="4" className="form-control" placeholder="Password" />
                           </div>
                           <div className="form-group">
-                            <input type="password" name="confirm-password" id="confirm-password" tabIndex="2" className="form-control" placeholder="Confirm Password" />
+                            <input type="password" ref="confirmpassword" id="confirmpassword" tabIndex="4" className="form-control" placeholder="Confirm Password" />
+                          </div>
+                          <div className="form-group">
+                            <input type="text" ref="classkey" id="classkey" tabIndex="5" className="form-control" placeholder="Class Key" />
                           </div>
                           <div className="form-group">
                             <div className="row">
                               <div className="col-sm-6 col-sm-offset-3">
-                                <input type="submit" name="register-submit" id="register-submit" tabIndex="4" className="form-control btn btn-register" value="Register Now" />
+                                <input type="submit" ref="register-submit" id="register-submit" tabIndex="6" className="form-control btn btn-register" value="Register Now" />
                               </div>
                             </div>
                           </div>
+                          {this.state.error ? (
+                          <div className="alert">Invalid username or password.</div>
+                          ) : null }
                         </form>
                       </div>
                     </div>
@@ -190,15 +284,109 @@ var Student = React.createClass({
     );
   }
 });
+
+
+var auth = {
+    register: function(name, username, password, classkey, cb) {
+        // submit request to server, call the callback when complete
+        var url = "/register";
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            type: 'POST',
+            data: {
+                name: name,
+                username: username,
+                password: password
+            },
+            // on success, store a login token
+            success: function(res) {
+                localStorage.token = res.token;
+                localStorage.name = res.name;
+                if (cb)
+                    cb(true);
+                this.onChange(true);
+            }.bind(this),
+            error: function(xhr, status, err) {
+                // if there is an error, remove any login token
+                delete localStorage.token;
+                if (cb)
+                    cb(false);
+                this.onChange(false);
+            }.bind(this)
+        });
+    },
+    // login the user
+    login: function(username, password, cb) {
+        // submit login request to server, call callback when complete
+        cb = arguments[arguments.length - 1];
+        // check if token in local storage
+        if (localStorage.token) {
+            if (cb) {
+                cb(true);
+
+            }
+            this.onChange(true);
+            return;
+        }
+
+        // submit request to server
+        var url = "/login/verify";
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            type: 'POST',
+            data: {
+                username: username,
+                password: password
+            },
+            success: function(res) {
+                // on success, store a login token
+                localStorage.token = res.token;
+                localStorage.name = res.name;
+                if (cb)
+                    cb(true);
+                this.onChange(true);
+            }.bind(this),
+            error: function(xhr, status, err) {
+                // if there is an error, remove any login token
+                delete localStorage.token;
+                if (cb)
+                    cb(false);
+                this.onChange(false);
+            }.bind(this)
+        });
+    },
+    // get the token from local storage
+    getToken: function() {
+        return localStorage.token;
+    },
+    // get the name from local storage
+    getName: function() {
+        return localStorage.name;
+    },
+    // logout the user, call the callback when complete
+    logout: function(cb) {
+        delete localStorage.token;
+        if (cb) cb();
+        this.onChange(false);
+    },
+    // check if user is logged in
+    loggedIn: function() {
+        return !!localStorage.token;
+    },
+    // default onChange function
+    onChange: function() {},
+};
+
 var routes = (
   <Router>
     <Route path="/" component={App}>
       <Route path="recover" component={Recover}/>
+       <Route path="/ta" component={TA}/>
+    <Route path="/student" component={Student}/>
     </Route>
-    <Route path="/ta" component={TA}>
-    </Route>
-    <Route path="/student" component={Student}>
-    </Route>
+   
   </Router>
 );
 
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 // setup Express
 var app = require('./models/express.js');
 
+// setup mongoose
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/youngUsers');
+
 // start the server
+var api = require('./models/api.js');
+var User = require('./models/user.js');
+//var Item = require('./models/item.js');
+
 var server = app.listen(3000, function() {
 console.log("Started on port 3000");
 var host = server.address().address;
